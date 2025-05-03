6e93ff4424051568da836805235457e1abbb063b
ShivanHussain <shivanhussain6@gmail.com>
2025-05-03 23:41:20 +0530

    Update userRoute.js
    add authenticated

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
-router.post("/logout",isAuthenticated,logout);
+router.post("/logout",isAuthenticatedUser,logout);
 router.get("/admin/getall/:collegeName",getByCollegeName);
 router.get("/admin/user/getall",getALLUser);
 
